ab190b67837ad6cc9639fe009133715dc62b0c7c
abuzar316 <mohammadabuzar316@gmail.com>
2023-05-28 17:20:56 +0530

    chaneg

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import express from "express";
-import paymentRoutes from "./routes/paymentRoutes.js";
 import cors from "cors";
 
 const app = express();
 
 // use middleware
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 app.use(cors({ origin: "*" }));
 
-app.use("/api/v1", paymentRoutes);
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "server is running!" });
+});
 
 app.use((req, res) => {
   res.status(200).json({ message: "Page Not Found" });
